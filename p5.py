58092a946f02a845566fe9068e52bd15c02cdd30
heyscottn <heyscottn@gmail.com>
2024-02-20 19:14:04 -0600

    mm

---
+++ p5.py
@@ -0,0 +1,14 @@
+# https://codingquest.io/problem/18
+
+# technically we *do* know how many lines of input
+# but let's pretend we don't know how many lines of input
+
+# input to a list of strings
+# then figure out what to do with it
+
+
+
+
+# convert to dictionary
+# if key does not exist, make new key:value
+# if key does exist, total with previous
