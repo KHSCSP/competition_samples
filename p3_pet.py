0bf31a2638429324d1e4ebfaa6a142a1077f72f6
heyscottn <heyscottn@gmail.com>
2024-02-25 13:44:52 -0600

    mm

--- p3_pet.py
+++ p3_pet.py
@@ -1,7 +1,5 @@
 # https://open.kattis.com/problems/pet
 
-# https://open.kattis.com/problems/pet
-
 # more than one line of input, but known how many lines
 
 # get a line, add it up, add the total to a list
