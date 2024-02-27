57eaae6b066c779791e91213baec5ce0c911dec5
heyscottn <heyscottn@gmail.com>
2024-02-27 14:58:53 -0600

    mm

---
+++ asteroids.py
@@ -0,0 +1,31 @@
+# a helper function
+def show_field():
+    print()
+
+# the asteroid class
+
+
+
+
+# ----- the runnable code
+import sys, math
+# some variables
+
+
+
+# read the data in, append to list of asteroids
+
+
+
+
+# the first hour
+
+
+
+
+# the next minute
+
+
+
+
+
